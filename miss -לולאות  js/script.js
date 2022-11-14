1a48a802f91ea44c874c2d13ed8cf5f734558a9e
noycoohen <114675124+noycoohen@users.noreply.github.com>
2022-11-14 22:03:32 +0200

    Add files via upload

---
+++ miss -לולאות  js/script.js
@@ -0,0 +1,11 @@
+let array = [];
+
+for (let i = 1; i < 11; i++) {
+  let b = prompt("Write word");
+  console.log(b);
+}
+
+function displayArray(x) {
+  console.log(x);
+}
+array.forEach(displayArray);
